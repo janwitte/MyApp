126b53cbdddc5d5a64cc561948e81e5ca4ab6977
janwitte <janwitte@hotmail.de>
2013-08-28 23:44:55 +0200

    First Commit
    Just Started (http://docs.sencha.com/touch/2.2.1/#!/guide/first_app)

---
+++ app.js
@@ -0,0 +1,45 @@
+/*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
+
+// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
+//@require @packageOverrides
+
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'touch/src'
+});
+//</debug>
+
+Ext.application({
+    name: 'Sencha',
+
+    launch: function() {
+        Ext.create("Ext.tab.Panel", {
+            fullscreen: true,
+            tabBarPosition: 'bottom',
+
+            items: [
+                {
+                    title: 'Home',
+                    iconCls: 'home',
+                    html: [
+                        '<img src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>You're creating the Getting Started app. This demonstrates how ",
+                        "to use tabs, lists, and forms to create a simple app</p>",
+                        '<h2>Sencha Touch</h2>'
+                    ].join("")
+                }
+            ]
+        });
+    }
+});
